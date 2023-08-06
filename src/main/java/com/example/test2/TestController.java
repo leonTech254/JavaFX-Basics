2247318f86c637c58f89020ca8495392c9818f79
leonTech254 <martinleontech23@gmail.com>
2023-08-06 15:40:05 +0300

    --modifying the element[click event]

--- src/main/java/com/example/test2/TestController.java
+++ src/main/java/com/example/test2/TestController.java
@@ -1,6 +1,7 @@
 package com.example.test2;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class TestController {
@@ -8,9 +9,13 @@ public class TestController {
     private Label mylabel;
 
     @FXML
+    private Button mybtnId;
+
+    @FXML
     private  void clickMeLeon()
     {
         System.out.println("Hello Leon,You just clicked me!");
+        mybtnId.setText("clicked");
     }
 
 }
